c3dec7026cccde9300e0fdd7515e498ec86d5be0
Ryan Au <auryan898@gmail.com>
2020-04-13 21:10:28 -0400

    Remove unused imports from ev3 testing program, which isn't very useful anymore

--- test/ca/mcgill/ecse211/ev3test/MainTester.java
+++ test/ca/mcgill/ecse211/ev3test/MainTester.java
@@ -2,24 +2,9 @@ package ca.mcgill.ecse211.ev3test;
 
 import static ca.mcgill.ecse211.project.LocalResources.*;
 
-import ca.mcgill.ecse211.project.Display;
 import ca.mcgill.ecse211.project.LocalResources;
-import ca.mcgill.ecse211.project.Localizer;
-import ca.mcgill.ecse211.project.Navigation;
-import ca.mcgill.ecse211.project.menu.MainMenu;
 import ca.mcgill.ecse211.tools.SubMenu;
-import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.motor.Motor;
-import lejos.hardware.motor.NXTRegulatedMotor;
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.hardware.sensor.EV3UltrasonicSensor;
-import lejos.robotics.RegulatedMotor;
-import lejos.robotics.chassis.Chassis;
-import lejos.robotics.chassis.Wheel;
-import lejos.robotics.chassis.WheeledChassis;
-import lejos.robotics.localization.OdometryPoseProvider;
-import lejos.robotics.navigation.MovePilot;
 
 /**
  * The main class that includes basic initialization methods for initializing
