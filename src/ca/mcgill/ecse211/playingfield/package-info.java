3177447af951e1c6ce40421317b80ceb7098461f
Ryan Au <auryan898@gmail.com>
2020-04-14 16:16:48 -0400

    Generated preliminary javadoc for final submission

--- src/ca/mcgill/ecse211/playingfield/package-info.java
+++ src/ca/mcgill/ecse211/playingfield/package-info.java
@@ -1,5 +1,12 @@
-
 /**
+ * Playing Field Parameters
+ * This package classes that can represent the parameters of the competition
+ * playing field.
+ * 
+ * <p>
+ * Use the Point class to represent single points on the map, such as the
+ * corners of a Region. Each Region is defined by the Point of its lower left
+ * corner, and the point of its upper right corner.
  * 
  */
 package ca.mcgill.ecse211.playingfield;
--- src/ca/mcgill/ecse211/project/package-info.java
+++ src/ca/mcgill/ecse211/project/package-info.java
@@ -1,5 +1,14 @@
-
 /**
+ * Project Package
+ * This package contains the main classes most commonly used in controlling the
+ * robot.
  * 
+ * <p>
+ * It contains the Main class, which is to be run at the start of the program,
+ * and uses @see ca.mcgill.ecse211.project.menu.MainMenu by default to display
+ * different methods to control the robot. It also contains the ever-important
+ * LocalResources class, which stores static references to every important
+ * resources, such as wifi parameters, navigation, localization, and other
+ * important logic.
  */
 package ca.mcgill.ecse211.project;
